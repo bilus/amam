af806405b626adac1a4fcc41a0e3f03b34253a5c
Marcin Bilski <gyamtso@gmail.com>
2019-11-03 11:48:40 +0100

    Add validation for food.

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save food without name" do
+    food = Food.new
+    assert_not food.save
+  end
+
+  test "should save valid food" do
+    food = Food.new
+    food.name = "foo bar"
+    assert food.save
+  end
 end
