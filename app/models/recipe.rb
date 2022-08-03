a6c83a52283e0b3701382e5ec0771cc51463db58
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 12:46:08 +0800

    Delete app directory

--- app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ApplicationRecord
-  has_many :ingredients, dependent: :destroy
-  has_many :foods, through: :ingredients
-
-  accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: ->(attr) { attr['food_id'].nil? }
-end
--- app/views/ingredients/_ingredient.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! ingredient, :id, :recipe_id, :food_id, :serving, :created_at, :updated_at
-json.url ingredient_url(ingredient, format: :json)
